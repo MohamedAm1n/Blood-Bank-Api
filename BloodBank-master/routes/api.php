110348b3ce839402ca831eb68e743aacd34b9c79
amin <maamin97@yahoo.com>
2020-09-07 22:23:09 +0200

    v1

--- BloodBank-master/routes/api.php
+++ BloodBank-master/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// V1
 Route::group(['prefix' => 'v1', 'namespace' => 'Api'], function () {
     Route::get('governorates', 'MainController@governorates');
     Route::get('cities', 'MainController@cities');
